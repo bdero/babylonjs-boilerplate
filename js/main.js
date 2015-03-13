424c0e149c6111bacea8e173e02408c6330d4721
Brandon DeRosier <bdero@mit.edu>
2015-03-12 23:54:02 -0400

    Add Oimo.js for physics
    - Enable gravity
    - First person camera
    - Enable collisions between camera, floor, and box
    - Rearrange objects

--- js/main.js
+++ js/main.js
@@ -3,33 +3,45 @@
   var Engine = new BABYLON.Engine(Canvas, true);
   var Scene = new BABYLON.Scene(Engine);
 
-  var Camera = new BABYLON.ArcRotateCamera(
-    "Camera", 1, 2, 20,
-    new BABYLON.Vector3(0, 0, 0),
+  Scene.enablePhysics();
+  Scene.gravity = new BABYLON.Vector3(0, -9.81, 0);
+  Scene.collisionsEnabled = true;
+
+  var Camera = new BABYLON.FreeCamera(
+    "FreeCamera",
+    new BABYLON.Vector3(0, 5, -15),
     Scene
   );
+  Camera.checkCollisions = true;
+  Camera.ellipsoid = new BABYLON.Vector3(1, 1, 1);
+  Camera.applyGravity = true;
 
   var light0 = new BABYLON.PointLight(
     "Omni",
-    new BABYLON.Vector3(0, 0, 10),
+    new BABYLON.Vector3(0, 18, -5),
     Scene
   );
 
-  var ball = BABYLON.Mesh.CreateSphere("Ball", 10, 1.0, Scene);
+  var floor = BABYLON.Mesh.CreateBox("Floor", 50, Scene);
+  floor.position.y = 0;
+  floor.scaling.y = 0.1;
+  floor.checkCollisions = true;
+
+  var ball = BABYLON.Mesh.CreateSphere("Ball", 20, 8, Scene);
   ball.position.x = -5;
+  ball.position.y = 1;
+  ball.setPhysicsState({
+    impostor: BABYLON.PhysicsEngine.BoxImposter,
+    mass: 1,
+    friction: 0.5,
+    restitution: 0.7
+  });
 
-  var box = BABYLON.Mesh.CreateBox("Cube", 1, Scene);
-  box.position.x = -10;
-
-  var plane = BABYLON.Mesh.CreatePlane("Plane", 10, Scene);
-  plane.rotation.y = Math.PI;
-  plane.position.x = 5;
-
-  var cylinder = BABYLON.Mesh.CreateCylinder(
-    "Cylinder", 3, 1, 3, 20,
-    Scene, false
-  );
-  cylinder.position.x = 15;
+  var box = BABYLON.Mesh.CreateBox("Cube", 2, Scene);
+  box.position.x = 5;
+  box.position.y = 3.5;
+  box.position.z = -5;
+  box.checkCollisions = true;
 
   Scene.activeCamera.attachControl(Canvas);
 
