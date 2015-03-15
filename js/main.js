821ac8fe6d1a802d56e8826f7992a0fdd55b3548
Brandon DeRosier <bdero@mit.edu>
2015-03-14 23:23:05 -0400

    Just use lowercase names for everything

--- js/main.js
+++ js/main.js
@@ -1,33 +1,33 @@
 (function() {
-  var Canvas = document.getElementById("renderCanvas");
-  var Engine = new BABYLON.Engine(Canvas, true);
-  var Scene = new BABYLON.Scene(Engine);
+  var canvas = document.getElementById("renderCanvas");
+  var engine = new BABYLON.Engine(canvas, true);
+  var scene = new BABYLON.Scene(engine);
 
-  Scene.enablePhysics();
-  Scene.gravity = new BABYLON.Vector3(0, -9.81, 0);
-  Scene.collisionsEnabled = true;
+  scene.enablePhysics();
+  scene.gravity = new BABYLON.Vector3(0, -9.81, 0);
+  scene.collisionsEnabled = true;
 
-  var Camera = new BABYLON.FreeCamera(
+  var camera = new BABYLON.FreeCamera(
     "FreeCamera",
     new BABYLON.Vector3(0, 5, -15),
-    Scene
+    scene
   );
-  Camera.checkCollisions = true;
-  Camera.ellipsoid = new BABYLON.Vector3(1, 1, 1);
-  Camera.applyGravity = true;
+  camera.checkCollisions = true;
+  camera.ellipsoid = new BABYLON.Vector3(1, 1, 1);
+  camera.applyGravity = true;
 
   var light0 = new BABYLON.PointLight(
     "Omni",
     new BABYLON.Vector3(0, 18, -5),
-    Scene
+    scene
   );
 
-  var floor = BABYLON.Mesh.CreateBox("Floor", 50, Scene);
+  var floor = BABYLON.Mesh.CreateBox("Floor", 50, scene);
   floor.position.y = 0;
   floor.scaling.y = 0.1;
   floor.checkCollisions = true;
 
-  var ball = BABYLON.Mesh.CreateSphere("Ball", 20, 8, Scene);
+  var ball = BABYLON.Mesh.CreateSphere("Ball", 20, 8, scene);
   ball.position.x = -5;
   ball.position.y = 1;
   ball.setPhysicsState({
@@ -37,28 +37,28 @@
     restitution: 0.7
   });
 
-  var box = BABYLON.Mesh.CreateBox("Cube", 2, Scene);
+  var box = BABYLON.Mesh.CreateBox("Cube", 2, scene);
   box.position.x = 5;
   box.position.y = 3.5;
   box.position.z = -5;
   box.checkCollisions = true;
 
-  Scene.activeCamera.attachControl(Canvas);
+  scene.activeCamera.attachControl(canvas);
 
   var prevTime, currentTime, deltaTime = 0;
   prevTime = currentTime = Date.now();
 
-  Engine.runRenderLoop(function() {
+  engine.runRenderLoop(function() {
     currentTime = Date.now();
     deltaTime = (currentTime - prevTime)/1000;
     prevTime = currentTime;
 
     box.rotation.y += 1*deltaTime;
 
-    Scene.render();
+    scene.render();
   });
 
   window.addEventListener("resize", function() {
-    Engine.resize();
+    engine.resize();
   });
 })();
